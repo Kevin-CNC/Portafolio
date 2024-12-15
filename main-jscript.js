1e37159d900a20f180df21607ed67bd21f6ca40c
Kevin-CNC <165025856+Kevin-CNC@users.noreply.github.com>
2024-12-15 05:26:30 +0000

    Update main-jscript.js

--- main-jscript.js
+++ main-jscript.js
@@ -1,47 +1,40 @@
-let title = document.getElementById("title");
-let sub_title = document.getElementById("little-title");
+// JavaScript to handle dashboard animation
+const showDashboardBtn = document.getElementById('showAboutMe');
+const showSkillsDashboardBtn = document.getElementById('showSkills');
+const showProjectsDashboardBtn = document.getElementById('showProjects');
+const showContacts = document.getElementById('showContacts');
+const dashboard = document.getElementById('dashboard');
+const skillboard = document.getElementById('skillsDashboard');
+const projectboard = document.getElementById('showProjectsboard');
+const contactboard = document.getElementById('showContactsboard');
+const overlay = document.getElementById('overlay');
 
-const title_text = "Kevin Caplescu"
-const sub_text="UK-Based, Italian web developer and Computer Science student!"
+// Dashboard Buttons
+showDashboardBtn.addEventListener('click', () => {
+  dashboard.classList.add('visible');
+  overlay.classList.add('active');
+});
 
-async function loop1() {
-    for (let i = 0; i < title_text.length; i++) {
-        title.innerHTML+=title_text[i];
-        await new Promise(resolve => setTimeout(resolve, 15));
-    }
-}
+showSkillsDashboardBtn.addEventListener('click', () => {
+    skillboard.classList.add('visible');
+    overlay.classList.add('active');
+});
 
-async function loop2() {
-    for (let o = 0; o < sub_text.length; o++) {
-        sub_title.innerHTML+=sub_text[o];
-        await new Promise(resolve => setTimeout(resolve, 25)); 
-    }
-}
+showProjectsDashboardBtn.addEventListener('click', () => {
+    projectboard.classList.add('visible');
+    overlay.classList.add('active');
+});
 
-async function backgroundLoop(){
-    let transparency_mode = true
-    let transp = .4
-    const Widget = document.getElementById("bg-fade");
-    Widget.style.backgroundColor = `rgba(132, 194, 171, ${transp})`
-    while(true){
-        for (let x = 0; x <7; x++){
-            if(transparency_mode){
-                transp+=.1;
-            }else{
-                transp-=.1;
-            };
+showContacts.addEventListener('click', () => {
+    contactboard.classList.add('visible');
+    overlay.classList.add('active');
+});
 
-            Widget.style.backgroundColor = `rgba(132, 194, 171, ${transp})`
-            await new Promise(resolve => setTimeout(resolve, 250)); 
-        };
-        
-        if(transparency_mode){transparency_mode=false}else{transparency_mode=true};
-    };
-}
-
-// running them both parallel
-async function runLoopsInParallel() {
-    await Promise.all([loop1(), loop2(), backgroundLoop()]);
-}
-
-runLoopsInParallel();
+// Close Dashboard when clicking on overlay
+overlay.addEventListener('click', () => {
+  dashboard.classList.remove('visible');
+  skillboard.classList.remove('visible');
+  projectboard.classList.remove('visible');
+  contactboard.classList.remove('visible');
+  overlay.classList.remove('active');
+});
